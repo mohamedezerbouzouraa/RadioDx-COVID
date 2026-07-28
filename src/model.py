56cc1418b6cc2579e26c3fd85ef8ad3ec06a04e4
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-07-28 22:54:10 +0100

    Add model building function with ResNet18

--- src/model.py
+++ src/model.py
@@ -1,7 +1,6 @@
 import torch
 import torchvision
 
-
 def build_model(num_classes=3, learning_rate=3e-5):
     model = torchvision.models.resnet18(pretrained=True)
     model.fc = torch.nn.Linear(in_features=512, out_features=num_classes)
