7031df74c061750f15d831d34f143176a3b17b8c
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-07-29 23:10:34 +0100

    build_model function

--- src/model.py
+++ src/model.py
@@ -4,8 +4,6 @@ import torchvision
 def build_model(num_classes=3, learning_rate=3e-5):
     model = torchvision.models.resnet18(pretrained=True)
     model.fc = torch.nn.Linear(in_features=512, out_features=num_classes)
-
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
     return model, loss_fn, optimizer
